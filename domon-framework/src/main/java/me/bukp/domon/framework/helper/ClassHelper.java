bfd4892270a146ec4f64bc93bf4124d1de2be481
bukp_at_home <lconline@163.com>
2016-08-26 23:15:25 +0800

    完成class helper

---
+++ domon-framework/src/main/java/me/bukp/domon/framework/helper/ClassHelper.java
@@ -0,0 +1,76 @@
+package me.bukp.domon.framework.helper;
+
+import me.bukp.domon.framework.annotation.Controller;
+import me.bukp.domon.framework.annotation.Service;
+import me.bukp.domon.framework.util.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 类操作助手类
+ * <p>
+ * Created by bukp on 2016/8/26.
+ */
+public final class ClassHelper {
+
+    private static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    /**
+     * 得到应用程序包名下的所有类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    /**
+     * 获取应用包名下的所有的Service类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getServiceClassSet() {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Service.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+
+    /**
+     * 获取应用包名下所有Controller类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getControllerClassSet() {
+        Set<Class<?>> classSet = new HashSet<>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Controller.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    /**
+     * 得到应用包名下所有bean类
+     *
+     * @return
+     */
+    public static Set<Class<?>> getBeanClassSet() {
+        Set<Class<?>> classSet = new HashSet<>();
+        classSet.addAll(getServiceClassSet());
+        classSet.addAll(getControllerClassSet());
+        return classSet;
+    }
+
+}
