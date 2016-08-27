fa0e9a3650ed486098c3f066d93ab0b5d1e6ca65
bukp <lconline@163.com>
2016-08-27 18:33:43 +0800

    完成bean助手类

---
+++ domon-framework/src/main/java/me/bukp/domon/framework/helper/BeanHelper.java
@@ -0,0 +1,53 @@
+package me.bukp.domon.framework.helper;
+
+import me.bukp.domon.framework.util.ReflectionUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Bean 助手类
+ * Created by bukp on 2016/8/27.
+ */
+public final class BeanHelper {
+
+
+    /**
+     * bean映射，存放bean类与bean实例的映射关系
+     */
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap<Class<?>, Object>();
+
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for (Class<?> beanClass : beanClassSet) {
+            Object obj = ReflectionUtil.newInstance(beanClass);
+            BEAN_MAP.put(beanClass, obj);
+        }
+    }
+
+    /**
+     * 获取bean映射
+     *
+     * @return
+     */
+    public static Map<Class<?>, Object> getBeanMap() {
+        return BEAN_MAP;
+    }
+
+    /**
+     * 获取bean实例
+     *
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> cls) {
+        if (!BEAN_MAP.containsKey(cls)) {
+            throw new RuntimeException("can not get bean by class: " + cls);
+        }
+        return (T) BEAN_MAP.get(cls);
+    }
+
+
+}
