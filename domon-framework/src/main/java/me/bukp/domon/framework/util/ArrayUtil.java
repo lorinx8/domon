81ea98334d975a5bf06f6cf85aee2912ae64b2f1
bukp_at_home <lconline@163.com>
2016-08-28 19:22:25 +0800

    完成数组工具类

---
+++ domon-framework/src/main/java/me/bukp/domon/framework/util/ArrayUtil.java
@@ -0,0 +1,33 @@
+package me.bukp.domon.framework.util;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ * 数组工具类
+ * <p>
+ * Created by bukp on 2016/8/28.
+ */
+public final class ArrayUtil {
+
+    /**
+     * 判断数据是否非空
+     *
+     * @param array
+     * @return
+     */
+    public static boolean isNotEmpty(Object[] array) {
+        return !ArrayUtils.isEmpty(array);
+    }
+
+    /**
+     * 判断数组是否为空
+     *
+     * @param array
+     * @return
+     */
+    public static boolean isEmpty(Object[] array) {
+        return ArrayUtils.isEmpty(array);
+    }
+
+
+}
